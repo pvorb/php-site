7e2b26cea07847408e99915bfa585dbe1e680690
Paul Vorbach <paul@vorb.de>
2011-04-08 22:42:56 +0200

    Adapted mapper.

--- example/www/__mapper.php
+++ example/www/__mapper.php
@@ -6,4 +6,11 @@
  * @author Paul Vorbach <paul@vorb.de>
  */
 
+// Check if $uri is the init script or a mapper and throw a 404 response.
+$uri = $request->getUri();
+if ($uri == '__init.php' || $uri == MAPPER_FILE) {
+	throw new HttpResponse(404);
+}
 
+// Define current directory as DIR_MAPPER.
+define('DIR_MAPPER', dirname(__FILE__));
